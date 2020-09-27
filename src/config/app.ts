a00e6c6d7799b82c58cdb34612fa6ca5e15f36ac
typeleven <typeleven@gmail.com>
2020-09-27 16:07:01 -0600

    0.0.0.0

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,7 +1,7 @@
 const app = {
     port: Number(process.env.PORT) || 9000,
     env: process.env.NODE_ENV || 'development',
-    host: process.env.HOST_NAME || 'localhost',
+    host: process.env.HOST_NAME || '0.0.0.0',
 
     bodySizeLimit: process.env.API_BODY_SIZE_LIMIT || '100mb',
     parameterLimit: Number(process.env.API_PARAMETER_LIMIT) || 10000,
