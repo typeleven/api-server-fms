96cea06bd98004795c43efb5018c02282309369b
typeleven <typeleven@gmail.com>
2020-11-14 12:17:13 -0700

    added auth to s3 endpoints

--- src/api/aws/s3/index.ts
+++ src/api/aws/s3/index.ts
@@ -9,21 +9,26 @@ const upload = multer();
 
 const router = express.Router();
 
-router.post('/:bucket/:dir', upload.single('file'), async (req, res) => {
-    try {
-        if (!req.file) return res.boom.badRequest('File is required');
-        const key = await aws.upload(
-            req.file,
-            req.params.dir,
-            req.params.bucket
-        );
-        res.send({ key });
-    } catch (error) {
-        res.boom.badRequest(error.message);
+router.post(
+    '/:bucket/:dir',
+    services.auth.validation,
+    upload.single('file'),
+    async (req, res) => {
+        try {
+            if (!req.file) return res.boom.badRequest('File is required');
+            const key = await aws.upload(
+                req.file,
+                req.params.dir,
+                req.params.bucket
+            );
+            res.send({ key });
+        } catch (error) {
+            res.boom.badRequest(error.message);
+        }
     }
-});
+);
 
-router.get('/:bucket', async (req, res) => {
+router.get('/:bucket', services.auth.validation, async (req, res) => {
     try {
         const key = req.query.key;
         const result: any = await aws.get(
