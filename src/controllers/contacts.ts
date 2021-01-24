6656e2153d5ca62f9cb467c06628d14d141246ad
Eden Morris <typeleven@gmail.com>
2021-01-24 16:35:08 -0600

    Refactor contacts

--- src/controllers/contacts.ts
+++ src/controllers/contacts.ts
@@ -1,50 +1,21 @@
 import Contact from '../models/contacts';
 
 const create = async (data: object) => {
-    try {
-        const contact = new Contact(data);
-        await contact.save();
-        return contact;
-    } catch (error) {
-        throw new Error(error);
-    }
+    await Contact.create(data);
 };
 
 const get = async (_id: string) => {
-    try {
-        const result = await Contact.findOne({ _id }).populate('attachments');
-        return result;
-    } catch (error) {
-        throw new Error(error);
-    }
+    await Contact.get(_id, 'attachments');
 };
 
 const list = async () => {
-    try {
-        const result = await Contact.find({}).populate('attachments');
-        return result;
-    } catch (error) {
-        throw new Error(error);
-    }
+    await Contact.list('attachments');
 };
 
-const update = async (_id: string, data: object) => {
-    try {
-        const update = await Contact.findOneAndUpdate({ _id }, data);
-        const result = await Contact.findOne({ _id });
-        return result;
-    } catch (error) {
-        throw new Error(error);
-    }
-};
+const update = async (_id: string, data: object) => Contact.update(_id, data);
 
 const remove = async (_id: string) => {
-    try {
-        const result = await Contact.findOneAndDelete({ _id });
-        return result;
-    } catch (error) {
-        throw new Error(error);
-    }
+    await Contact.remove({ _id });
 };
 
 export default { create, remove, update, get, list };
--- src/models/contacts.ts
+++ src/models/contacts.ts
@@ -23,4 +23,16 @@ contactSchema.set('toJSON', { virtuals: true });
 
 const Contact = model('Contact', contactSchema);
 
-export default Contact;
+const get = (_id: string, populate?: string | object) =>
+    Contact.findOne({ _id }).populate(populate);
+
+const list = (populate?: string | object) => Contact.find().populate(populate);
+
+const create = (data) => new Contact(data).save();
+
+const update = async (_id, data) =>
+    Contact.findByIdAndUpdate(_id, data, { new: true });
+
+const remove = (_id) => Contact.findOneAndDelete({ _id });
+
+export default { get, list, create, update, remove };
